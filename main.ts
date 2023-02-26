a03a127e9ac7c1a4ea19030ac76173c6636dd052
dhendrawirawan <119018562+dhendrawirawan@users.noreply.github.com>
2023-02-26 08:30:21 -0600

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,8 @@
 namespace SpriteKind {
     export const EnemyWeakspot = SpriteKind.create()
     export const SuperProjectile = SpriteKind.create()
+    export const AttackingPlayer = SpriteKind.create()
+    export const DefendingPlayer = SpriteKind.create()
 }
 sprites.onOverlap(SpriteKind.SuperProjectile, SpriteKind.Enemy, function (sprite, otherSprite) {
     if (CountWeakSpot == 0) {
@@ -10,96 +12,68 @@ sprites.onOverlap(SpriteKind.SuperProjectile, SpriteKind.Enemy, function (sprite
     sprites.destroy(sprite, effects.trail, 100)
 })
 controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
-    animation.runImageAnimation(
-    mySprite,
-    assets.animation`Hadoken`,
-    150,
-    false
-    )
-    Projectile2 = sprites.createProjectileFromSide(assets.image`Hadouken`, 100, 0)
-    Projectile2.setKind(SpriteKind.SuperProjectile)
-    Projectile2.setPosition(-3, 28)
+    timer.throttle("action", 500, function () {
+        controller.moveSprite(mySprite, 0, 0)
+        if (controller.up.isPressed()) {
+            mySprite.setKind(SpriteKind.DefendingPlayer)
+            animation.runImageAnimation(
+            mySprite,
+            assets.animation`HighBlock`,
+            150,
+            false
+            )
+        } else if (controller.down.isPressed()) {
+        	
+        } else {
+            mySprite.setKind(SpriteKind.AttackingPlayer)
+            animation.runImageAnimation(
+            mySprite,
+            assets.animation`Punch`,
+            150,
+            false
+            )
+            mySprite.setPosition(mySprite.x + 3, mySprite.y)
+        }
+    })
+    timer.after(1000, function () {
+        controller.moveSprite(mySprite, 30, 20)
+        mySprite.setKind(SpriteKind.Player)
+    })
 })
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    if (controller.up.isPressed()) {
-        animation.runImageAnimation(
-        mySprite,
-        assets.animation`HighBackKick`,
-        100,
-        false
-        )
-        mySprite.setPosition(mySprite.x + 3, mySprite.y)
-        projectile = sprites.createProjectileFromSprite(img`
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . 4 4 . . . . . . . 
-            . . . . . . 4 5 5 4 . . . . . . 
-            . . . . . . 2 5 5 2 . . . . . . 
-            . . . . . . . 2 2 . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            `, mySprite, 50, -25)
-    } else if (controller.down.isPressed()) {
-        animation.runImageAnimation(
-        mySprite,
-        assets.animation`AxeKick`,
-        100,
-        false
-        )
-        mySprite.setPosition(mySprite.x + 3, mySprite.y)
-        projectile = sprites.createProjectileFromSprite(img`
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . 4 4 . . . . . . . 
-            . . . . . . 4 5 5 4 . . . . . . 
-            . . . . . . 2 5 5 2 . . . . . . 
-            . . . . . . . 2 2 . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            `, mySprite, 50, 25)
-    } else {
-        animation.runImageAnimation(
-        mySprite,
-        assets.animation`LowKick`,
-        80,
-        false
-        )
-        mySprite.setPosition(mySprite.x + 5, mySprite.y)
-        projectile = sprites.createProjectileFromSprite(img`
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . 4 4 . . . . . . . 
-            . . . . . . 4 5 5 4 . . . . . . 
-            . . . . . . 2 5 5 2 . . . . . . 
-            . . . . . . . 2 2 . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            `, mySprite, 50, 0)
-    }
+    timer.throttle("action", 500, function () {
+        controller.moveSprite(mySprite, 0, 0)
+        mySprite.setKind(SpriteKind.AttackingPlayer)
+        if (controller.up.isPressed()) {
+            animation.runImageAnimation(
+            mySprite,
+            assets.animation`HighBackKick`,
+            100,
+            false
+            )
+            mySprite.setPosition(mySprite.x + 3, mySprite.y)
+        } else if (controller.down.isPressed()) {
+            animation.runImageAnimation(
+            mySprite,
+            assets.animation`AxeKick`,
+            100,
+            false
+            )
+            mySprite.setPosition(mySprite.x + 3, mySprite.y)
+        } else {
+            animation.runImageAnimation(
+            mySprite,
+            assets.animation`LowKick`,
+            80,
+            false
+            )
+            mySprite.setPosition(mySprite.x + 5, mySprite.y)
+        }
+        timer.after(500, function () {
+            controller.moveSprite(mySprite, 30, 20)
+            mySprite.setKind(SpriteKind.Player)
+        })
+    })
 })
 controller.right.onEvent(ControllerButtonEvent.Released, function () {
     animation.runImageAnimation(
@@ -121,12 +95,11 @@ sprites.onOverlap(SpriteKind.Projectile, SpriteKind.EnemyWeakspot, function (spr
     sprites.destroy(otherSprite, effects.spray, 500)
     CountWeakSpot += -1
 })
-sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
+sprites.onOverlap(SpriteKind.AttackingPlayer, SpriteKind.Enemy, function (sprite, otherSprite) {
     if (CountWeakSpot == 0) {
         statusbar.value += -1
     }
     myEnemy.x += 5
-    sprites.destroy(sprite, effects.trail, 100)
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
     while (sprite.overlapsWith(otherSprite)) {
@@ -135,20 +108,14 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSp
     }
     statusbar2.value += -1
 })
-let projectile: Sprite = null
-let Projectile2: Sprite = null
+let CountWeakSpot = 0
 let statusbar2: StatusBarSprite = null
 let statusbar: StatusBarSprite = null
-let CountWeakSpot = 0
 let myEnemy: Sprite = null
 let mySprite: Sprite = null
 info.startCountdown(60)
-tiles.setCurrentTilemap(tilemap`level2`)
-scene.setBackgroundImage(assets.image`InfinityTKD`)
-game.splash("Don't touch the enemy. Use the projectile from your three different kicks to hit all three of enemy's weak spots (the fire horn, crystal ball, and tri-fork tail) then you can damage the enemy.", "")
 music.play(music.createSong(assets.song`ImagineDragon1`), music.PlaybackMode.LoopingInBackground)
 mySprite = sprites.create(assets.image`AthleteBlue`, SpriteKind.Player)
-tiles.placeOnTile(mySprite, tiles.getTileLocation(2, 3))
 mySprite.z = 1
 mySprite.setStayInScreen(true)
 animation.runImageAnimation(
@@ -157,9 +124,8 @@ assets.animation`BlueNeutral`,
 150,
 true
 )
-controller.moveSprite(mySprite, 30, 0)
+controller.moveSprite(mySprite, 30, 20)
 myEnemy = sprites.create(assets.image`Godzillow`, SpriteKind.Enemy)
-tiles.placeOnTile(myEnemy, tiles.getTileLocation(5, 3))
 myEnemy.setStayInScreen(true)
 myEnemy.setScale(0.8, ScaleAnchor.Middle)
 animation.runImageAnimation(
@@ -168,49 +134,6 @@ assets.animation`Godzilowanime`,
 400,
 true
 )
-let WeakSpotTop = sprites.create(img`
-    4 . . . . . . . . . . . 
-    4 4 4 4 . . . . . . . . 
-    4 4 2 4 4 . . . . . . . 
-    . 4 2 2 4 4 . . . . . . 
-    . 4 2 2 2 4 4 . . . . . 
-    . . 4 2 2 2 2 4 4 . . . 
-    . . 4 4 2 2 2 4 4 4 . . 
-    . . . 4 2 4 4 4 4 4 4 . 
-    . . . 4 4 4 4 4 4 4 4 2 
-    . . . . 4 4 4 4 4 4 4 4 
-    . . . . 4 4 4 4 4 4 4 . 
-    . . . . . 4 4 4 2 . . . 
-    `, SpriteKind.EnemyWeakspot)
-let WeakspotMiddle = sprites.create(img`
-    . . . 9 9 9 9 9 9 . . . 
-    . . 9 9 9 8 9 9 9 9 . . 
-    . 9 9 8 8 8 9 9 9 9 9 . 
-    9 9 8 8 8 6 9 9 9 9 9 9 
-    9 8 8 6 6 9 9 9 9 9 9 9 
-    9 6 6 6 9 9 9 9 9 9 9 9 
-    9 9 9 9 9 9 9 9 9 9 9 9 
-    9 9 9 9 9 9 9 9 9 9 9 9 
-    9 9 9 9 9 9 9 9 9 9 9 9 
-    . 9 9 9 9 9 9 9 9 9 9 . 
-    . . 9 9 9 9 9 9 9 9 . . 
-    . . . 9 9 9 9 9 9 . . . 
-    `, SpriteKind.EnemyWeakspot)
-let WeakSpotBottom = sprites.create(img`
-    . . . . . . . e . . . . . . . 
-    . . . . . . e e e . . . . . . 
-    . . . . . . e e e . . . . . . 
-    . . . . . . e e e . . . . . . 
-    e . . . . e e b e e . . . . e 
-    e e . . . e c b e e . . . e e 
-    . e e . e e c e b e e . e e . 
-    . e e e e e c e b e e e e e . 
-    . . e e e c c e b e e e e . . 
-    . . e e e c c e b b e e e . . 
-    . . . e e c e e b b e e . . . 
-    . . . . . c e e e b . . . . . 
-    `, SpriteKind.EnemyWeakspot)
-CountWeakSpot = 3
 statusbar = statusbars.create(4, 20, StatusBarKind.Health)
 statusbar.attachToSprite(myEnemy, -10, -5)
 statusbar.positionDirection(CollisionDirection.Right)
@@ -228,54 +151,4 @@ game.onUpdate(function () {
 })
 game.onUpdateInterval(10, function () {
     myEnemy.x += -0.2
-    WeakSpotTop.setPosition(myEnemy.x + -15, myEnemy.y + -28)
-    WeakspotMiddle.setPosition(myEnemy.x + -16, myEnemy.y + 4)
-    WeakSpotBottom.setPosition(myEnemy.x + -24, myEnemy.y + 16)
-})
-game.onUpdateInterval(8000, function () {
-    if (CountWeakSpot == 0) {
-        WeakSpotTop = sprites.create(img`
-            4 . . . . . . . . . . . 
-            4 4 4 4 . . . . . . . . 
-            4 4 2 4 4 . . . . . . . 
-            . 4 2 2 4 4 . . . . . . 
-            . 4 2 2 2 4 4 . . . . . 
-            . . 4 2 2 2 2 4 4 . . . 
-            . . 4 4 2 2 2 4 4 4 . . 
-            . . . 4 2 4 4 4 4 4 4 . 
-            . . . 4 4 4 4 4 4 4 4 2 
-            . . . . 4 4 4 4 4 4 4 4 
-            . . . . 4 4 4 4 4 4 4 . 
-            . . . . . 4 4 4 2 . . . 
-            `, SpriteKind.EnemyWeakspot)
-        WeakspotMiddle = sprites.create(img`
-            . . . 9 9 9 9 9 9 . . . 
-            . . 9 9 9 8 9 9 9 9 . . 
-            . 9 9 8 8 8 9 9 9 9 9 . 
-            9 9 8 8 8 6 9 9 9 9 9 9 
-            9 8 8 6 6 9 9 9 9 9 9 9 
-            9 6 6 6 9 9 9 9 9 9 9 9 
-            9 9 9 9 9 9 9 9 9 9 9 9 
-            9 9 9 9 9 9 9 9 9 9 9 9 
-            9 9 9 9 9 9 9 9 9 9 9 9 
-            . 9 9 9 9 9 9 9 9 9 9 . 
-            . . 9 9 9 9 9 9 9 9 . . 
-            . . . 9 9 9 9 9 9 . . . 
-            `, SpriteKind.EnemyWeakspot)
-        WeakSpotBottom = sprites.create(img`
-            . . . . . . . e . . . . . . . 
-            . . . . . . e e e . . . . . . 
-            . . . . . . e e e . . . . . . 
-            . . . . . . e e e . . . . . . 
-            e . . . . e e b e e . . . . e 
-            e e . . . e c b e e . . . e e 
-            . e e . e e c e b e e . e e . 
-            . e e e e e c e b e e e e e . 
-            . . e e e c c e b e e e e . . 
-            . . e e e c c e b b e e e . . 
-            . . . e e c e e b b e e . . . 
-            . . . . . c e e e b . . . . . 
-            `, SpriteKind.EnemyWeakspot)
-        CountWeakSpot = 3
-    }
 })
